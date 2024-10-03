d01243a5b6ac246614600c0ebd5dba37d739699b
aryan8gupta <aryan8sunny@gmail.com>
2024-10-03 17:32:29 +0530

    h

--- Inventify/settings.py
+++ Inventify/settings.py
@@ -130,7 +130,7 @@ STATICFILES_DIRS = [
 # [
 #     BASE_DIR, "/static/"
 # ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type

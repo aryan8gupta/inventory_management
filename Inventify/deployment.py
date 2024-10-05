b548d5ef2d5c324c6431226674d3bf4517078187
aryan8gupta <aryan8sunny@gmail.com>
2024-10-06 01:42:17 +0530

    Updated Settings

---
+++ Inventify/deployment.py
@@ -0,0 +1,42 @@
+import os 
+import pymongo
+from .settings import *
+from .settings import BASE_DIR
+
+ALLOWED_HOSTS = ['*']
+CSRF_TRUSTED_ORIGINS = ['https://inventory-management-app-hnbvgqaag3g2hyde.centralindia-01.azurewebsites.net/']
+DEBUG = False
+SECRET_KEY = 'django-insecure-$j$mcen!5kw_vxos#q9g)cm!@zflik7k3&nohn@rg_&qx!88hl'
+PUBLIC_KEY = 'Aryan971pass'
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'https://inventory-management-app-hnbvgqaag3g2hyde.centralindia-01.azurewebsites.net/' 
+]
+
+
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
+
+CONNECTION = pymongo.MongoClient('mongodb://localhost:27017')
+DB = CONNECTION.Inventory_Management
+
+
+STATIC_ROOT = BASE_DIR/'staticfiles'
--- Inventify/wsgi.py
+++ Inventify/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Inventify.settings')
+settings_module = 'Inventify.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'Inventify.settings'
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()

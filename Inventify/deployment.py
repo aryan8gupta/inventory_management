49c91776fad124180604cb1a337d53db7cfaa6b4
aryan8gupta <aryan8sunny@gmail.com>
2024-10-11 18:11:26 +0530

    ""

--- Inventify/deployment.py
+++ Inventify/deployment.py
@@ -53,8 +53,8 @@ from pymongo import MongoClient
 # connection_string = "mongodb://inventory-management-app-server.mongo.cosmos.azure.com:443/"
 connection_string = "mongodb://inventory-management-app-server:{os.getenv('Azure_Cosmos_Pswd')}@inventory-management-app-server.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@inventory-management-app-server@"
 
-client = MongoClient(connection_string)
 print(connection_string)
+client = MongoClient(connection_string)
 DB = client['inventory-management-app-database']
 
 

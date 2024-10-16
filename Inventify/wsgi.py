24081e82910ad451499d4338ed8f759d22e0f3f8
aryan8gupta <aryan8sunny@gmail.com>
2024-10-16 08:08:50 +0530

    TEST RUN 100

--- Inventify/wsgi.py
+++ Inventify/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-settings_module = 'Inventify.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'Inventify.settings'
+settings_module = 'Inventify.deployment' if 'https://inventory-management-app-hnbvgqaag3g2hyde.centralindia-01.azurewebsites.net/' in os.environ else 'Inventify.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
 
 application = get_wsgi_application()
